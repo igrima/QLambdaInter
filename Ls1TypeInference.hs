fda9f920e96bb2a41a67c2a4ba004a17acb6328a
nsm <smnicolas@gmail.com>
2021-03-14 12:19:25 -0300

    Agrego el caso QIf para decorateLs1 (incompleto).
    Para tipos QBit (o sea B^n) se chequea la ortigonalidad facilmente.
    Queda implementar para S(B^n).

--- Ls1TypeInference.hs
+++ Ls1TypeInference.hs
@@ -6,7 +6,7 @@
 -- License: GNU General Public License, v2
 -- License Official Site: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 -- ---------------------------------------------------------------------------------------------------------- //
--- Copyright (c) 2017  Nicolás San Martín based in code developed by Fidel <fidel -at- unq.edu.ar >
+-- Copyright (c) 2017  Nicolás San Martín
 -- 
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -40,7 +40,23 @@ import QEnvironments
 import QTsTypeInference
 
 
---valuesAreOrthogonal :: (BaseQT a) -> (BaseQT a) -> Bool
+isValidTypeForQIf :: QType -> Bool
+isValidTypeForQIf (TSup _) = False -- TODO: not implemented yet
+isValidTypeForQIf x = isBaseQBitN x
+
+
+canComputeOrthogonality :: BaseQT a -> Bool
+canComputeOrthogonality (QBit _)      = True
+canComputeOrthogonality (Prod ts _)   = all canComputeOrthogonality ts
+canComputeOrthogonality _             = False
+
+areOrthogonal :: Eq a => (BaseQT a) -> (BaseQT a) -> Bool
+areOrthogonal (QBit x) (QBit y) = x /= y
+areOrthogonal (Prod ts _) (Prod tt _) = ts /= tt
+areOrthogonal (LC v _) (LC w _) = True --TODO: implement this!
+areOrthogonal (LC mt _) _ = False
+areOrthogonal _ (LC mt _) = False
+areOrthogonal _ _ = False
 
 ---------------------------------------------------------
 -- inferType
@@ -48,18 +64,25 @@ import QTsTypeInference
 decorateLs1 r = getResValue (runQTM (decorateTermLs1 emptyEnv r))
 
 decorateTermLs1 env r = do
-  (chr,_) <- deduceType env r
+  (chr,_) <- deduceTypeLs1 env r
   return chr
 
 deduceTypeLs1 :: Ord a => Environment -> BaseQT a -> QTMonad (ChurchQTerm, QType)
-
 deduceTypeLs1 env (Null t)  = raise ("There is no Null vector")
-deduceTypeLs1 env (QIf t r _) =
+deduceTypeLs1 env (QIf t r x) =
    do (cht, tt) <- deduceTypeLs1 env t
       (chr, tr) <- deduceTypeLs1 env r
-      ta        <- produceCompatibleTypeFor tt tr 
-                     ("Branch Types not compatible in QIf:\n" ++ show tt ++ "\n" ++ show tr)
-      tqif      <- qifType ta
-      return (QIf cht chr tqif, tqif)
+      if not (isValidTypeForQIf tt) || not (isValidTypeForQIf tr)
+        then raise ("Branch Types not compatible in QIf:\n" ++ show tt ++ "\n" ++ show tr)
+        else
+        do
+          if (areOrthogonal cht chr)
+            then
+            do
+              ta <- produceCompatibleTypeFor tt tr 
+                    ("Branch Types not compatible in QIf:\n" ++ show tt ++ "\n" ++ show tr)
+              tqif <- qifType ta
+              return (QIf cht chr tqif, tqif)
+            else raise ("Branch Types must be orthogonal")
 
 deduceTypeLs1 a b = deduceType a b
